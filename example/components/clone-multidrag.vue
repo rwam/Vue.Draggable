4fed4d5de49b2b2cd1e3f5f0f654267ef9d04e1c
Ralf Richtsteiger <dev@rwam.de>
2019-04-12 16:47:28 +0200

    add multidrag support

---
+++ example/components/clone-multidrag.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="row">
+    <div class="col-3">
+      <h3>Draggable 1</h3>
+      <draggable
+        class="dragArea list-group"
+        :list="list1"
+        :group="{ name: 'people', pull: 'clone', put: false }"
+        multi-drag="true"
+        selected-class="selected"
+        @change="log"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list1"
+          :key="element.name"
+        >
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+
+    <div class="col-3">
+      <h3>Draggable 2</h3>
+      <draggable
+        class="dragArea list-group"
+        :list="list2"
+        :options="{
+          multiDrag: true,
+          selectedClass: 'selected'
+        }"
+        group="people"
+        @change="log"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list2"
+          :key="element.name"
+        >
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+
+    <rawDisplayer class="col-3" :value="list1" title="List 1" />
+
+    <rawDisplayer class="col-3" :value="list2" title="List 2" />
+  </div>
+</template>
+
+<script>
+import draggable from "@/vuedraggable";
+export default {
+  name: "clone-multidrag",
+  display: "Clone Multidrag",
+  order: 2,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list1: [
+        { name: "John", id: 1 },
+        { name: "Joao", id: 2 },
+        { name: "Jean", id: 3 },
+        { name: "Gerard", id: 4 }
+      ],
+      list2: [
+        { name: "Juan", id: 5 },
+        { name: "Edgard", id: 6 },
+        { name: "Johnson", id: 7 }
+      ]
+    };
+  },
+  methods: {
+    log: function(evt) {
+      window.console.log(evt);
+    }
+  }
+};
+</script>
+<style scoped>
+.selected {
+  background-color: #f9c7c8;
+}
+</style>
